649d0c5f01c3b47c87863d323901baf5540eb936
Filip Merta <filipmerta.fm@gmail.com>
2021-01-14 21:21:01 +0100

    TitleScreen

---
+++ game/source/menu/TitleScreen.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_NewGame_pressed():
+	pass # Replace with function body.
+
+
+func _on_LoadGame_pressed():
+	pass # Replace with function body.
+
+
+func _on_Settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	pass # Replace with function body.
