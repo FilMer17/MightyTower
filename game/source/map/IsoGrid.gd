12d6e7ba3af66a9dba5ae6c9afc3b52c4a245109
Filip Merta <filipmerta.fm@gmail.com>
2021-01-17 11:46:54 +0100

    IsoGrid static class

--- game/source/map/IsoGrid.gd
+++ game/source/map/IsoGrid.gd
@@ -15,7 +15,44 @@ const NEIGHBOR_TABLE := [
 	Vector2(-1, 0), # NW
 ]
 
-static func map_to_world(cell: Vector2) -> Vector2:
-	var x: int = int(cell.x) * int(CELL_SIZE.x)
-	var y: int = int(cell.y) * int(CELL_SIZE.y)
+static func map_to_world(cell: Vector2, size: Vector2) -> Vector2:
+	var x: int = int(cell.x) * int(CELL_SIZE.x * size.x)
+	var y: int = int(cell.y) * int(CELL_SIZE.y * size.y)
+	
 	return Vector2(x, y)
+
+static func world_to_map(position: Vector2, size: Vector2) -> Vector2:
+# warning-ignore:integer_division
+	var x: int = int(position.x) / int(CELL_SIZE.x * size.x)
+# warning-ignore:integer_division
+	var y: int = int(position.y) / int(CELL_SIZE.y * size.y)
+	
+	return Vector2(x, y)
+
+static func get_neighbor(cell: Vector2, direction: int) -> Vector2:
+	var neighbor: Vector2 = cell + NEIGHBOR_TABLE[direction]
+	
+	return neighbor
+
+static func get_all_neighbors(cell: Vector2) -> PoolVector2Array:
+	var neighbors: PoolVector2Array = []
+	
+	for direction in NEIGHBOR_TABLE:
+		var neighbor_cell: Vector2 = cell + direction
+		neighbors.append(neighbor_cell)
+	
+	return neighbors
+
+static func get_area_around(cell: Vector2, scale: int) -> PoolVector2Array:
+	var area_around: PoolVector2Array = []
+	
+	for neighbor_direction in NEIGHBOR_TABLE:
+		var neighbor: Vector2 = cell
+		var temp: Vector2 = neighbor
+# warning-ignore:unused_variable
+		for i in range(0, scale):
+			temp = neighbor
+			neighbor = temp + neighbor_direction
+			area_around.append(neighbor)
+	
+	return area_around
