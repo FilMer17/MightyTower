05abe7c3e98aa9bcf6db0cd93749f5d233d1590f
Filip Merta <filipmerta.fm@gmail.com>
2021-01-17 15:09:36 +0100

    GameCamera

--- game/source/menu/TitleScreen.gd
+++ game/source/menu/TitleScreen.gd
@@ -12,14 +12,14 @@ func _on_Settings_pressed():
 func _on_Quit_pressed():
 	pass # Replace with function body.
 
-var mouse_pos
-var nm = Vector2(0,0)
-func _process(_delta):
-	mouse_pos = get_global_mouse_position()
-	if nm == mouse_pos:
-		return
-	else:
-		print(IsoGrid.world_to_map(mouse_pos, Vector2(3, 3)))
-		print(IsoGrid.get_area_around(IsoGrid.world_to_map(mouse_pos, Vector2(3, 3)), 2))
-	
-	nm = mouse_pos
+#var mouse_pos
+#var nm = Vector2(0,0)
+#func _process(_delta):
+#	mouse_pos = get_global_mouse_position()
+#	if nm == mouse_pos:
+#		return
+#	else:
+#		print(IsoGrid.world_to_map(mouse_pos, Vector2(3, 3)))
+#		print(IsoGrid.get_area_around(IsoGrid.world_to_map(mouse_pos, Vector2(3, 3)), 2))
+#
+#	nm = mouse_pos
