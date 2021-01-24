4360c0cb0497238725c5600f11273e4b45a979df
Filip Merta <filipmerta.fm@gmail.com>
2021-01-24 22:29:24 +0100

    Script GameWorld loading game data

--- game/source/global/Global.gd
+++ game/source/global/Global.gd
@@ -1,4 +1,8 @@
 extends Node
 
 # String(name of world) : Bool(is_new)
-var selected_world: Dictionary = {}
+var selected_world: Dictionary = {
+	"world1" : true,
+	"map_size" : 0,
+	"difficulty" : 0
+	}
---
+++ game/source/world/GameWorld.gd
@@ -0,0 +1,33 @@
+extends Node2D
+class_name GameWorld
+
+export var alias: String = ""
+
+onready var settings := $Settings
+onready var resources := $Resources
+onready var build_grid := $BuilderGrid
+onready var map := $MapContainer/Map
+onready var buildings := $BuildingContainer/Buildings
+
+var sel_world: Dictionary = {}
+var world_data: Dictionary = {}
+var path := "user://worlds/"
+
+func _ready() -> void:
+	_load_game_data()
+
+func _load_game_data() -> void:
+	sel_world = Global.selected_world
+	alias = sel_world.keys()[0] 
+
+	if sel_world[alias]:
+		pass
+	else:
+#		path += alias + ".save"
+#		world_data = Data.load_data(path)
+
+#		settings.data = world_data["settings"]
+#		resources.data = world_data["resources"]
+#		map.data = world_data["map"]
+#		buildings.data = world_data["buildings"]
+		pass
