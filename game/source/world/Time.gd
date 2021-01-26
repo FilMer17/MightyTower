59e4b6d833c9515d1d2f1f995bd6be588eecce56
Filip Merta <filipmerta.fm@gmail.com>
2021-01-26 21:16:05 +0100

    Script Time clock ticking

--- game/source/world/Time.gd
+++ game/source/world/Time.gd
@@ -1,28 +1,31 @@
 extends Node
 
-export var year: int = 0
-export var month: int = 0
-export var day: int = 0
+export var year: int = 1
+export var month: int = 1
+export var day: int = 1
 export var hour: int = 0
 export var minute: int = 0
-export var paused: bool = false
 
-var wait_time: int = 1.0
-var time: Timer = null
+onready var clock: Timer = $Clock
 
 func _ready():
-	time = _set_timer(wait_time)
-	time.start()
+	clock.start()
 
-
-func _set_timer(_wait_time: float) -> Timer:
-	var _timer = Timer.new()
-	add_child(_timer)
-	var __ = _timer.connect("timeout", self, "_on_Time_timeout")
-	_timer.set_wait_time(_wait_time)
-	_timer.set_one_shot(false) # Make sure it loops
-	
-	return _timer
-
-func _on_Time_timeout() -> void:
-	pass
+func _on_Clock_timeout() -> void:
+	minute += 1
+	if minute >= 60:
+		minute = 0
+		hour += 1
+		# call hour changes -> building time
+		if hour >= 24:
+			hour = 0
+			day += 1
+			# call day changed -> get resources
+			if day >= 31:
+				day = 1
+				month += 1
+				# call month changed -> new month 
+				if month >= 13:
+					month = 1
+					year += 1
+					# call year changes -> new year
