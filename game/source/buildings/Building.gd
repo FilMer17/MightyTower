80bfdc04595da371a7d030d392ea98e678142b71
Filip Merta <filipmerta.fm@gmail.com>
2021-02-06 17:29:19 +0100

    Structure Building first FoodStorage

--- game/source/buildings/Building.gd
+++
@@ -1,3 +0,0 @@
-extends Node2D
-
-export var data: Resource = null # BuildingTData
---
+++ game/source/buildings/tdata/StorageTData.gd
@@ -0,0 +1,7 @@
+extends BuildingTData
+class_name StorageTData
+
+enum USAGE { food, material, ore}
+
+export(USAGE) var usage: int = 0
+export var max_store: int = 0
