369bba2d055320342f88cec4afa7f37f79eb3716
Filip Merta <filipmerta.fm@gmail.com>
2021-02-12 19:05:41 +0100

    Data WorldData beginning

---
+++ game/source/data/WorldData.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name WorldData
+
+export var settings: Resource = null
+export var time: Resource = null
+export var resources: Resource = null
+export var map: Resource = null
