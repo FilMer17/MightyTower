91dc6d1fe01dd2094a8d12ea449fa5f8f1b80b25
Filip Merta <filipmerta.fm@gmail.com>
2021-02-13 22:28:40 +0100

    Script World name moved to script

--- game/source/data/WorldData.gd
+++ game/source/data/WorldData.gd
@@ -1,7 +1,6 @@
 extends Resource
 class_name WorldData
 
-export var name: String = ""
 export var settings: Dictionary = {}
 export var time: Dictionary = {}
 export var resources: Dictionary = {}
--- game/source/world/World.gd
+++ game/source/world/World.gd
@@ -7,28 +7,30 @@ onready var time = $Time
 onready var resources = $Resources
 onready var map = $Map
 
+var w_name: String = ""
+
 func _ready() -> void:
-	_create_world_data("S")
+	if GlobalData.selected_world["is_new"]:
+		_create_world_data("S")
 	
-#	if GlobalData.selected_world["is_new"]:
-#		pass
-#	else:
-#		data = GlobalData.selected_world["world"]
-#		_load_world_data()
+	else:
+		data = GlobalData.selected_world["world"]
+		_load_world_data()
 
 func _create_world_data(diffic: String) -> void:
 	# settings default
 	# time default
+	w_name = GlobalData.selected_world["name"]
 	resources.create_data(diffic)
 	map.create_data(diffic)
 
+
 func _load_world_data() -> void:
+	w_name = GlobalData.selected_world["name"]
 	settings.load_data(data.settings)
 	time.load_data(data.time)
 	resources.load_data(data.resources)
 	map.load_data(data.map)
-	
-	data.name = GlobalData.selected_world["name"]
 
 func _save_world_data() -> void:
 	data.settings = settings.save_data(data.settings)
